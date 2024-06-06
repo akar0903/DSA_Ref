3cfb1c38d21faf0a5c0660ebd7bb69cc25139031
akar0903 <104198301+akar0903@users.noreply.github.com>
2024-06-06 22:03:15 +0530

    deletelastelementarray.cs

---
+++ deletelastarray.cs
@@ -0,0 +1,18 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    int[] intarr = new int[9];
+    int length=0;
+    for(int i=0;i<6;i++){
+        intarr[length]=i;
+        length++;
+    }
+    for(int i=length;i<1;i--){
+        intarr[i-1]=intarr[i];
+    }
+    length--;
+    for(int i=0;i<length;i++){
+        Console.WriteLine(intarr[i]);
+    }
+  }
+}
